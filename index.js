898aad3b3257749c7ede53996fe7a1925ed5d550
Yah'Mir McGhee <yahmir.mcghee@gmail.com>
2022-02-23 16:39:50 -0500

    Index.js, index.html, and style.css has been created

---
+++ index.js
@@ -0,0 +1,15 @@
+// javascript
+var btn = document.querySelector(".btn"); 
+btn.addEventListener('click', function select(e) {
+    console.log("Go to Tutor Page");
+});
+
+var btn2 = document.querySelector(".btn2");
+btn2.addEventListener('click', function select(e) {
+    console.log("Go To Videos Page");
+});
+
+var btn3 = document.querySelector(".btn3");
+btn3.addEventListener('click',function select(e) {
+    console.log("Go To Games Page");
+});
